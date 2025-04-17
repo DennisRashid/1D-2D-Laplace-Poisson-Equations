62c6d8443f40a7350550aacf00eac7a3c7656028
Dennis Rashid <151940745+DennisRashid@users.noreply.github.com>
2025-04-17 14:12:43 +0300

    Initial commit: 1D Poisson equation finite difference solver

---
+++ 1D_Poisson_Eqn.py
@@ -0,0 +1,36 @@
+#%%
+import numpy as np
+from scipy import interpolate
+import scipy.linalg as la
+import matplotlib.pyplot as plt
+
+N = 5 #Discretization
+u0, u1 = 1, 2 #Boundary Conditions
+dx = 1/(N+1) #Grid spacing
+
+#Construct the system matrix A (Laplacian operator using finite differences)
+A = (np.eye(N, k=-1) - 2*np.eye(N, k=0) + np.eye(N, k=1))/(dx**2) 
+
+#Define the right-hand side vector b for Poisson's equation: u_xx = -5
+b = -5 * np.ones(N) #RHS values for interior points (source term is -5)
+
+#Modify b to include the effect of Dirichlet boundary conditions
+b[0] = -5 - u0/dx**2 #Influence of u(0)
+b[-1] = -5 - u1/dx**2 #Influence of u(1)
+
+#Solve the linear system
+u = la.solve(A, b)
+
+x = np.linspace(0, 1, N+2)
+U = np.hstack([[u0], u, [u1]]) #Include boundary values to get the full solution
+U_x = interpolate.interp1d(x, U, axis=0, kind='cubic') #Interpolate for smooth plotting
+
+#Plot the solution
+fig, ax = plt.subplots(figsize=(8, 4))
+
+ax.plot(x, U)
+ax.plot(x[1:-1], u, 'ks')
+ax.set_xlim(0, 1)
+ax.set_xlabel(r'$x$', fontsize=18)
+ax.set_ylabel(r'$U(x)$', fontsize=18)
+# %%
